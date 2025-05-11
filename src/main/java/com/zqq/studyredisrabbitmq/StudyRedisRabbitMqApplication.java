8bab8c5791efbb51689e46f737f5fcb50ddb5f57
PwnDurant <1781389019@qq.com>
2025-05-11 09:35:08 +0800

    first commit

---
+++ src/main/java/com/zqq/studyredisrabbitmq/StudyRedisRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.zqq.studyredisrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRedisRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRedisRabbitMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zqq/studyredisrabbitmq/StudyRedisRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqq.studyredisrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRedisRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
